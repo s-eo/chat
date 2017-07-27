b50107e6d5d80e62c3ca5a8c584f27e1ae384ce7
Skakovskaya Elena <eoskakovskaya@mail.ru>
2017-07-27 14:41:42 +0300

    send chat message event is working

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # chat
 chat is based on sockets.io
+
+written followed by instructions from 
+https://socket.io/get-started/chat/
--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var opn = require('opn');
 
+
+app.use(express.static('public/static'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
-  opn('http://localhost:3000/', {
-    app: 'firefox'
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
   });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3000, function(){
+  /*opn('http://localhost:3000/', {
+    app: 'firefox'
+  });*/
   console.log('listening on *:3000');
 });
